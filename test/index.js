43df153f57d98ca2f3e8bc554f2c4404a7139f39
Alexander <alykoshin@gmail.com>
2016-05-05 21:07:42 +0300

    first version

--- test/index.js
+++ test/index.js
@@ -6,30 +6,67 @@ var chai = require('chai');
 var assert = chai.assert;
 var expect = chai.expect;
 chai.should();
-chai.use(require('chai-things')); //http://chaijs.com/plugins/chai-things
+//chai.use(require('chai-things')); //http://chaijs.com/plugins/chai-things
+
+var request = require('supertest');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var MiniRestMonitoringProcess = require('../lib/');
 
 
 describe('mini-rest-monitoring-process', function () {
 
-  before('before', function () {
+  describe('exports', function () {
+    it('expect to export function', function () {
+      expect(typeof MiniRestMonitoringProcess === 'function');
+    });
 
+    it('expect init() to return function', function () {
+      expect(typeof MiniRestMonitoringProcess() === 'function');
+    });
   });
 
-  beforeEach('beforeEach', function () {
 
-  });
+  describe('expect to return proper HTTP response', function () {
+    var app, next;
 
-  afterEach('afterEach', function () {
+    before('before', function() {
+      app = express();
+      app.use(bodyParser.json());
+      app.use('/', MiniRestMonitoringProcess());
 
-  });
+      next = function() { throw new Error('next() must never be called'); };
+    });
 
-  after('after', function () {
+    it('expect to return valid HTTP response for parent handler', function (done) {
+      request(app).get('/')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect(function(res) {
+          console.log('res.body:' + JSON.stringify(res.body,null,2));
+          expect(res.body).to.have.all.keys(MiniRestMonitoringProcess.keys);
+        })
+        .end(done)
+      ;
+    });
 
-  });
+    MiniRestMonitoringProcess.keys.forEach(function(key) {
 
-  it('should have unit test', function () {
-
-    assert(false, 'Please add unit tests.');
+    it('expect to return valid HTTP response for each separate handler: /'+key, function (done) {
+        request(app).get('/'+key)
+          .set('Accept', 'application/json')
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect(function(res) {
+            console.log('res.body:' + JSON.stringify(res.body,null,2));
+            //expect(res.body).to.have.all.keys(key);
+          })
+          .end(done)
+        ;
+      });
+    });
 
   });
 
